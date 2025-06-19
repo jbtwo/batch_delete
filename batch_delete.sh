0f141a130bd35d49d39466d3a3c42ba0f333cc72
Justin Brown <justinbrown@JBs-MacBook-Air.local>
2025-06-19 12:35:09 -0400

    use access token instead of key+secret

--- batch_delete.sh
+++ batch_delete.sh
@@ -5,7 +5,11 @@ source options.txt
 file=input.txt
 lines=$(cat ${file})
 for line in ${lines}; do
-   curl -X $METHOD --retry 5 "https://$API_KEY:$API_SECRET@$STORE_NAME.myshopify.com/admin/api/$API_VERSION/$RESOURCE/${line}.json"
+   curl 
+      -X $METHOD \
+      --retry 5 \
+      -H "X-Shopify-Access-Token:$ACCESS_TOKEN" \
+      "https://$STORE_NAME.myshopify.com/admin/api/$API_VERSION/$RESOURCE/${line}.json"
 done
 IFS=""
 exit ${?}
